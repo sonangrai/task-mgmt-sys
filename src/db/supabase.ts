6d672d76b711da17c0291b0be3666abc377fa3c0
sonangrai <shonahangrae@gmail.com>
2025-11-02 10:41:45 +0545

    feat: added supabase inst

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://rddzkxizeppagxxdczmn.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
