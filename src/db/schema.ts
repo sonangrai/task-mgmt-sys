813b84bd631ed2711e9fdfb0387e64107eb5c5b9
sonangrai <shonahangrae@gmail.com>
2025-11-02 10:42:09 +0545

    feat: updated the user schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,12 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const users = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  email: varchar("email").notNull().unique(),
+  supabaseUid: uuid("supabase_uid").notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
 });
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
