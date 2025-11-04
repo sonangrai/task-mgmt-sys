6df27c00a308c92b171a850b7ee64936e89fac39
sonangrai <shonahangrae@gmail.com>
2025-11-04 19:54:15 +0545

    feat: aded response class and updated response

---
+++ src/controller/response.ts
@@ -0,0 +1,26 @@
+/**
+ * A response class
+ *  msg = the message of the response
+ *  data = data object
+ *  status = the response status
+ */
+class ResponseObj {
+  status: number;
+  data: object | null;
+  meta: object | null;
+  msg: string;
+
+  constructor(
+    status: number,
+    data: object | null,
+    meta: object | null,
+    msg: string
+  ) {
+    this.status = status;
+    this.data = data;
+    this.meta = meta;
+    this.msg = msg;
+  }
+}
+
+export default ResponseObj;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,49 +1,23 @@
 // routes/users.js
 import express, { Request, Response } from "express";
 import { authenticateToken } from "../middleware/auth";
-import { db } from "../db/index.js";
-import { users } from "../db/schema.js";
-import { eq } from "drizzle-orm";
+import ResponseObj from "../controller/response";
 
 const router = express.Router();
 
 // Get current user profile
 router.get("/", authenticateToken, async (req: Request, res: Response) => {
   try {
-    res.send(req.user);
+    const resp = new ResponseObj(
+      200,
+      req.user as Object,
+      null,
+      "User data fetched successfully"
+    );
+    return res.send(resp);
   } catch (error) {
     return res.send(error);
   }
 });
-
-// Sync user on first login (called from frontend after auth)
-// router.post("/sync", authenticateToken, async (req: Request, res: Response) => {
-//   try {
-//     const { email } = req.body;
-
-//     // Check if user already exists
-//     const existingUser = await db
-//       .select()
-//       .from(users)
-//       .where(eq(users.supabaseUid, req?.user?.id));
-
-//     if (existingUser.length > 0) {
-//       return res.json(existingUser[0]);
-//     }
-
-//     // Create new user in your database
-//     const [newUser] = await db
-//       .insert(users)
-//       .values({
-//         supabaseUid: req?.user?.id,
-//         email: email,
-//       })
-//       .returning();
-
-//     res.json(newUser);
-//   } catch (error) {
-//     return res.send(error);
-//   }
-// });
 
 export default router;
