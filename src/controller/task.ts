01562153ad081de4ead3a1b609efb6100ff397bb
sonangrai <shonahangrae@gmail.com>
2025-11-09 10:43:57 +0545

    feat: added user check for the fetching of the task

--- src/controller/task.ts
+++ src/controller/task.ts
@@ -2,12 +2,21 @@ import { Request, Response } from "express";
 import { db } from "../db";
 import { task } from "../db/schema";
 import ResponseObj from "./response";
+import { eq } from "drizzle-orm";
 
 export const getTask = async (req: Request, res: Response) => {
   try {
-    const response = await db.select().from(task);
+    const response = await db
+      .select()
+      .from(task)
+      .where(eq(task.userId, req?.user?.id as string));
 
-    const responseObj = new ResponseObj(200, response, null, "Task fetched");
+    const responseObj = new ResponseObj(
+      200,
+      response,
+      null,
+      response.length > 0 ? "Task fetched" : "No Task for this user"
+    );
     return res.send(responseObj);
   } catch (error) {
     return res.send(error);
