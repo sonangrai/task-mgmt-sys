2f60f84173a292beb0a7ebb145f5147d845d57ba
sonangrai <shonahangrae@gmail.com>
2025-12-25 10:02:43 +0545

    feat: updated card design

--- frontend/src/components/task-card.tsx
+++ frontend/src/components/task-card.tsx
@@ -35,15 +35,17 @@ const getBadgeType = (pr: TTaskPriority): any => {
 function TaskCard({ data }: TaskCardProp) {
   return (
     <div className="p-1 gap-1 border rounded-sm px-2 py-1">
-      <h3 className="font-medium">{data.title}</h3>
-      <div className="overflow-hidden">
+      <h3 className="font-medium text-sm">{data.title}</h3>
+      <div className="overflow-hidden text-xs my-2 text-gray-500">
         <div
           dangerouslySetInnerHTML={{ __html: data.description }}
           className="inline-block max-h-10 overflow-hidden"
         />
       </div>
       <div>
-        <Badge variant={getBadgeType(data.priority)}>{data.priority}</Badge>
+        <Badge className="rounded-sm" variant={getBadgeType(data.priority)}>
+          {data.priority}
+        </Badge>
       </div>
     </div>
   )
